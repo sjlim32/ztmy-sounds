19c0545c592f6ed185bb4644177c5157e98ceedf
lsjoon <lsjoon.dev@gmail.com>
2026-08-20 20:42:07 +0900

    feat: 곡 목록 라우트 추가

---
+++ src/app/guide/page.tsx
@@ -0,0 +1,10 @@
+import { SongList } from "@/components/guide/SongList";
+
+export default function GuidePage() {
+  return (
+    <main data-role="guide-index">
+      <h1>Call Guide</h1>
+      <SongList />
+    </main>
+  );
+}
