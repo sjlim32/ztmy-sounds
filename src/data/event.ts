fe7a2c06ca08b2867f4556f47298ee559a5eb161
lsjoon <lsjoon.dev@gmail.com>
2026-08-20 20:41:56 +0900

    feat: 공연 이벤트 정보 데이터 추가

---
+++ src/data/event.ts
@@ -0,0 +1,16 @@
+export interface EventInfo {
+  artist: string;
+  tourName: string;
+  /** ISO 8601, e.g. "2026-12-31T18:00:00+09:00" */
+  showDateTime: string;
+  venue: string;
+  dateLabel: string;
+}
+
+export const event: EventInfo = {
+  artist: "ARTIST NAME",
+  tourName: "TOUR NAME 2026",
+  showDateTime: "2026-12-31T18:00:00+09:00",
+  venue: "VENUE NAME",
+  dateLabel: "12.31",
+};
